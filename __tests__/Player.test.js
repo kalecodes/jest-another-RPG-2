3b3907d9a4745005ddb0a70943cec24456b20363
kalen510 <kalen.wiley@gmail.com>
2023-05-02 21:27:01 -0400

    player properties and tests

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -14,3 +14,21 @@ test('creates a player object', () => {
         expect.arrayContaining([expect.any(Object)])
     );
 });
+
+test("gets the player's stats as an object", () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    // simulate an empty array to test if false is returned
+    player.inventory = [];
+    expect(player.getInventory()).toEqual(false);
+});
